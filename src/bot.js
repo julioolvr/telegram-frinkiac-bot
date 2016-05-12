a97a28b19df4fe6d60d513c11300afc8210e920a
Julio Olivera <julio.olvr@gmail.com>
2016-05-12 18:37:44 -0300

    Different respond methods

--- src/bot.js
+++ src/bot.js
@@ -56,56 +56,62 @@ export default class {
 
   respondTo(update) {
     if (update.message) {
-      let message = update.message;
-
-      if (!message.text) {
-        return;
-      }
-
-      if (message.text.startsWith('/start')) {
-        const startMessage = 'Use this bot inline to search a Simpson screenshot on frinkiac.com.\n' +
-                             'For example: @FrinkiacSearchBot d\'oh';
-
-        this.client.sendText(startMessage, message.chat.id);
-      }
-
-      if (message.text.startsWith('/help')) {
-        const helpMessage = 'This is an inline bot. This means that you can use it on any chat, private or group, without ' +
-                            'inviting it. Just type "@FrinkiacSearchBot <your search>" and wait. The bot will show you some ' +
-                            'screenshots matching your query, and you can select one of them. Try it here! Just make sure to ' +
-                            'add "@FrinkiacSearchBot" at the beginning of your message.\n\n' +
-                            'You can generate "meme" images by adding your own subtitle to the image. To do this, write your ' +
-                            'search query, and the text you want separated by a slash (/). For instance, "@FrinkiacSearchBot ' +
-                            'drugs lisa / give me the drugs, lisa" and then pick one of the thumbnails. The image will be ' +
-                            'generated with your text.';
-
-        this.client.sendText(helpMessage, message.chat.id);
-      }
+      this.respondToMessage(update.message);
     } else if (update.inline_query) {
-      if (!update.inline_query.query) {
-        return;
-      }
-
-      let queryParts = update.inline_query.query.split('/').map(s => s.trim());
-      let query = queryParts[0];
-      let caption = queryParts[1];
-
-      frinkiacApi.search(query).then(results => {
-        return results.map(result => {
-          let photoUrl = caption ? frinkiacApi.memeUrlFor(result, processCaption(caption)) : frinkiacApi.urlFor(result);
-
-          return {
-            type: 'photo',
-            id: guid(),
-            photo_url: photoUrl,
-            thumb_url: frinkiacApi.thumbnailUrlFor(result),
-            photo_width: SCREENSHOT_WIDTH,
-            photo_height: SCREENSHOT_HEIGHT
-          };
-        });
-      }).then(queryResults => {
-        return this.client.answerInlineQuery(update.inline_query.id, queryResults);
-      });
+      this.respondToInlineQuery(update.inline_query);
     }
   }
+
+   respondToMessage(message) {
+     if (!message.text) {
+       return;
+     }
+
+     if (message.text.startsWith('/start')) {
+       const startMessage = 'Use this bot inline to search a Simpson screenshot on frinkiac.com.\n' +
+                            'For example: @FrinkiacSearchBot d\'oh';
+
+       this.client.sendText(startMessage, message.chat.id);
+     }
+
+     if (message.text.startsWith('/help')) {
+       const helpMessage = 'This is an inline bot. This means that you can use it on any chat, private or group, without ' +
+                           'inviting it. Just type "@FrinkiacSearchBot <your search>" and wait. The bot will show you some ' +
+                           'screenshots matching your query, and you can select one of them. Try it here! Just make sure to ' +
+                           'add "@FrinkiacSearchBot" at the beginning of your message.\n\n' +
+                           'You can generate "meme" images by adding your own subtitle to the image. To do this, write your ' +
+                           'search query, and the text you want separated by a slash (/). For instance, "@FrinkiacSearchBot ' +
+                           'drugs lisa / give me the drugs, lisa" and then pick one of the thumbnails. The image will be ' +
+                           'generated with your text.';
+
+       this.client.sendText(helpMessage, message.chat.id);
+     }
+   }
+
+   respondToInlineQuery(inlineQuery) {
+     if (!inlineQuery.query) {
+       return;
+     }
+
+     let queryParts = inlineQuery.query.split('/').map(s => s.trim());
+     let query = queryParts[0];
+     let caption = queryParts[1];
+
+     frinkiacApi.search(query).then(results => {
+       return results.map(result => {
+         let photoUrl = caption ? frinkiacApi.memeUrlFor(result, processCaption(caption)) : frinkiacApi.urlFor(result);
+
+         return {
+           type: 'photo',
+           id: guid(),
+           photo_url: photoUrl,
+           thumb_url: frinkiacApi.thumbnailUrlFor(result),
+           photo_width: SCREENSHOT_WIDTH,
+           photo_height: SCREENSHOT_HEIGHT
+         };
+       });
+     }).then(queryResults => {
+       return this.client.answerInlineQuery(inlineQuery.id, queryResults);
+     });
+   }
 }
